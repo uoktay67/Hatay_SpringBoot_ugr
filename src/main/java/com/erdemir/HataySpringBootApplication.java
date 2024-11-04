477f3e9f790f9263cfa95648b85b9b9104ef0ea0
Uğur Oktay [Erdemir] <uoktay@isdemir.erdgrp.intra>
2024-11-04 11:54:08 +0300

    Initial commit

---
+++ src/main/java/com/erdemir/HataySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.erdemir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HataySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HataySpringBootApplication.class, args);
+	}
+
+}
